898f06d94d312eed77d6c9bf4b8a0fd1d021ada6
Dahyun Yi <35790641+dahyunyi@users.noreply.github.com>
2019-06-27 16:48:06 +0900

    Delete 0_prep_dtifit_to_baseline_t2_dyi.sh

--- 0_prep_dtifit_to_baseline_t2_dyi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-##for SubjID in $@; do 
-for SubjID in $(cat subjid_list.txt); do
-	flirt -in ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_S0.nii.gz -ref ~/Documents/DYI_project_processing/DTI/V0/${SubjID}_dtifit_V0_S0.nii.gz -out ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_S0.nii.gz;
-	flirt -in ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_FA.nii.gz -ref ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_S0.nii.gz -out ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_FA.nii.gz;
-	flirt -in ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_MD.nii.gz -ref ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_S0.nii.gz -out ~/Documents/DYI_project_processing/DTI/V2/${SubjID}_dtifit_V2_MD.nii.gz;
-done
