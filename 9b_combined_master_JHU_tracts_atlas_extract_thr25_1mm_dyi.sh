467b3ac4dc1c5d24c17fa2488a4cc91b34347ca9
Dahyun Yi <35790641+dahyunyi@users.noreply.github.com>
2019-06-27 16:49:17 +0900

    Delete 9b_combined_master_JHU_tracts_atlas_extract_thr25_1mm_dyi.sh

--- 9b_combined_master_JHU_tracts_atlas_extract_thr25_1mm_dyi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-bash JHU_tracts_atlas_extract_thr25_1mm_dyi.sh ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/FA/v0;
-bash JHU_tracts_atlas_extract_thr25_1mm_dyi.sh ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/FA/v2;
-bash JHU_tracts_atlas_extract_thr25_1mm_dyi.sh ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/MD/v0;
-bash JHU_tracts_atlas_extract_thr25_1mm_dyi.sh ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/MD/v2;
