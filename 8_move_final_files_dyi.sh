ab62877aff2ea8a4c7e490b90e2b7d16976654e6
Dahyun Yi <35790641+dahyunyi@users.noreply.github.com>
2019-06-27 16:49:03 +0900

    Delete 8_move_final_files_dyi.sh

--- 8_move_final_files_dyi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ${directory}
-find . -type f -name '*_V0_FA_warped2MNI152_T1_1mm.nii.gz' -exec cp {} ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/FA/v0/ \; 
-find . -type f -name '*_V0_MD_warped2MNI152_T1_1mm.nii.gz' -exec cp {} ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/MD/v0/ \; 
-find . -type f -name '*_V2_FA_warped2MNI152_T1_1mm.nii.gz' -exec cp {} ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/FA/v2/ \; 
-find . -type f -name '*_V2_MD_warped2MNI152_T1_1mm.nii.gz' -exec cp {} ~/Documents/DYI_project_processing/DTI/dti_post_ants_final/MD/v2/ \; 
